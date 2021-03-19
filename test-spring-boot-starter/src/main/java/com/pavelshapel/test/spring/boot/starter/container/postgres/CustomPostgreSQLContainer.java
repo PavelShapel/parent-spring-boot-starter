6dc410fa79fde7153cd47baaf703e5c1229a9b6e
pavelshapel <pavel.shapel@gmail.com>
2021-03-19 13:24:02 +0300

    add - CustomPostgreSQLContainer.java

---
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/postgres/CustomPostgreSQLContainer.java
@@ -0,0 +1,32 @@
+package com.pavelshapel.test.spring.boot.starter.container.postgres;
+
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class CustomPostgreSQLContainer extends PostgreSQLContainer<CustomPostgreSQLContainer> {
+    private static final String POSTGRES_IMAGE = "postgres:9.6.21";
+    private static CustomPostgreSQLContainer container;
+
+    private CustomPostgreSQLContainer() {
+        super(POSTGRES_IMAGE);
+    }
+
+    public static CustomPostgreSQLContainer getInstance() {
+        if (container == null) {
+            container = new CustomPostgreSQLContainer();
+        }
+        return container;
+    }
+
+    public static class PostgreSQLInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    String.format("spring.datasource.url=%s", container.getJdbcUrl()),
+                    String.format("spring.datasource.username=%s", container.getUsername()),
+                    String.format("spring.datasource.password=%s", container.getPassword())
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+}
