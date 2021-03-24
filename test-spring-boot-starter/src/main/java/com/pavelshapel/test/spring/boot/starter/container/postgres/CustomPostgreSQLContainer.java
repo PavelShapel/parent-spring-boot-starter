cecf05571a58cb9d514acf4b2a2a3667c30bee9b
pavelshapel <pavel.shapel@gmail.com>
2021-03-24 11:41:19 +0300

    refactor > CustomPostgreSQLContainer.java > add loggerLevel

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/postgres/CustomPostgreSQLContainer.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/postgres/CustomPostgreSQLContainer.java
@@ -1,10 +1,12 @@
 package com.pavelshapel.test.spring.boot.starter.container.postgres;
 
+import org.slf4j.event.Level;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.util.StringUtils;
 import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.Objects;
 
 public class CustomPostgreSQLContainer extends PostgreSQLContainer<CustomPostgreSQLContainer> {
     private static final String POSTGRES_IMAGE = "postgres:9.6.21";
@@ -14,14 +16,17 @@ public class CustomPostgreSQLContainer extends PostgreSQLContainer<CustomPostgre
         super(POSTGRES_IMAGE);
     }
 
-    public static CustomPostgreSQLContainer getInstance(String initScript) {
-        if (container == null) {
-            container = new CustomPostgreSQLContainer();
-        }
+    public static CustomPostgreSQLContainer getInstance(Level loggerLevel) {
+        container = new CustomPostgreSQLContainer();
 
-        return StringUtils.hasLength(initScript)
-                ? container.withInitScript(initScript)
-                : container;
+        return setLoggerLevel(loggerLevel);
+    }
+
+    private static CustomPostgreSQLContainer setLoggerLevel(Level loggerLevel) {
+        final String key = "loggerLevel";
+        return Objects.isNull(loggerLevel)
+                ? container.withUrlParam(key, Level.DEBUG.toString())
+                : container.withUrlParam(key, loggerLevel.toString());
     }
 
     public static CustomPostgreSQLContainer getInstance() {
