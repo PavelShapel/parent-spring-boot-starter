66d43c6451303dc302f84329efa428a2e615b752
pavelshapel <pavel.shapel@gmail.com>
2021-03-26 09:05:09 +0300

    refactor > StarterAutoConfiguration.java > add @ConditionalOnWebApplication on class

--- web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/StarterAutoConfiguration.java
+++ web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/StarterAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.pavelshapel.web.spring.boot.starter;
 
 import com.pavelshapel.web.spring.boot.starter.wrapper.TypedResponseWrapperRestControllerAdvice;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
+@ConditionalOnWebApplication
 public class StarterAutoConfiguration implements WebMvcConfigurer {
     public static final String TYPE = "web";
 
