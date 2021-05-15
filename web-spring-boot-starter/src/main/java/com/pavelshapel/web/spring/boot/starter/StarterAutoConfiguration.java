90c2b885cc0178a1691a2eb982ae437cb989c33a
Pavel, Shapel <pavel.shapel@ukg.com>
2021-05-15 17:13:24 +0300

    refactor > web-spring-boot-starter

--- web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/StarterAutoConfiguration.java
+++ web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/StarterAutoConfiguration.java
@@ -1,6 +1,8 @@
 package com.pavelshapel.web.spring.boot.starter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pavelshapel.web.spring.boot.starter.wrapper.TypedResponseWrapperRestControllerAdvice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,10 @@ import java.util.List;
 public class StarterAutoConfiguration implements WebMvcConfigurer {
     public static final String TYPE = "web";
 
+    //inject custom objectMapper to represent date/string correctly
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Bean
     public WebContextRefreshedListener webContextRefreshedListener() {
         return new WebContextRefreshedListener();
@@ -25,8 +31,17 @@ public class StarterAutoConfiguration implements WebMvcConfigurer {
         return new TypedResponseWrapperRestControllerAdvice();
     }
 
+
+    //override to represent date/string correctly
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(0, new MappingJackson2HttpMessageConverter());
+        converters.add(0, mappingJackson2HttpMessageConverter());
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        return converter;
     }
 }
