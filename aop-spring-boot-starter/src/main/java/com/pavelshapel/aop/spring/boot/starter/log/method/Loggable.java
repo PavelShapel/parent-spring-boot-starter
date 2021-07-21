858ad390fff290dd1c43e4c623ca1d328126c8a1
Pavel, Shapel <pavel.shapel@ukg.com>
2021-07-21 15:52:13 +0300

    refactor > aop-spring-boot-starter > all

--- aop-spring-boot-starter/src/main/java/com/pavelshapel/aop/spring/boot/starter/log/method/Loggable.java
+++ aop-spring-boot-starter/src/main/java/com/pavelshapel/aop/spring/boot/starter/log/method/Loggable.java
@@ -2,13 +2,12 @@ package com.pavelshapel.aop.spring.boot.starter.log.method;
 
 import org.slf4j.event.Level;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface Loggable {
     LoggableType[] value() default {LoggableType.METHOD_RESULT, LoggableType.METHOD_DURATION};
 
