9b37dc0d3b93022aa297bf8f832fd416d62064e8
Pavel, Shapel <pavel.shapel@ukg.com>
2021-07-25 10:15:53 +0300

    refactor > stream-spring-boot-starter > all

--- stream-spring-boot-starter/src/main/java/com/pavelshapel/stream/spring/boot/starter/StreamContextRefreshedListener.java
+++ stream-spring-boot-starter/src/main/java/com/pavelshapel/stream/spring/boot/starter/StreamContextRefreshedListener.java
@@ -1,12 +1,12 @@
 package com.pavelshapel.stream.spring.boot.starter;
 
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 import static com.pavelshapel.stream.spring.boot.starter.StarterAutoConfiguration.TYPE;
 
-@Log4j2
+@Slf4j
 public class StreamContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
