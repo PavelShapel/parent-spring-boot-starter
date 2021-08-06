f4953d6bd7513dd1f1914d3fc378c21bceed3883
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-06 11:06:36 +0300

    add > stream-spring-boot-starter > new collectors

--- stream-spring-boot-starter/src/main/java/com/pavelshapel/stream/spring/boot/starter/util/StreamUtils.java
+++ stream-spring-boot-starter/src/main/java/com/pavelshapel/stream/spring/boot/starter/util/StreamUtils.java
@@ -2,9 +2,8 @@ package com.pavelshapel.stream.spring.boot.starter.util;
 
 import org.springframework.http.ResponseEntity;
 
-import java.util.*;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -21,32 +20,5 @@ public class StreamUtils {
                 Collectors.toList(),
                 ResponseEntity::ok
         );
-    }
-
-    public <T> Collector<T, ?, List<T>> toList(boolean reverse) {
-        return Collectors.collectingAndThen(
-                Collectors.toList(), list -> {
-                    if (reverse) {
-                        Collections.reverse(list);
-                    }
-                    return list;
-                });
-    }
-
-    public <T, K, U> Collector<T, ?, Map<K, U>> toMapOfNullables(Function<? super T, ? extends K> keyMapper,
-                                                                 Function<? super T, ? extends U> valueMapper,
-                                                                 BinaryOperator<U> mergeFunction) {
-        return Collectors.collectingAndThen(
-                Collectors.toList(),
-                list -> {
-                    Map<K, U> result = new HashMap<>();
-                    for (T item : list) {
-                        K key = keyMapper.apply(item);
-                        U newValue = valueMapper.apply(item);
-                        U value = result.containsKey(key) ? mergeFunction.apply(result.get(key), newValue) : newValue;
-                        result.put(key, value);
-                    }
-                    return result;
-                });
     }
 }
