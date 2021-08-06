a37467979110f2074e3b2f4b2ee28b51c87b7392
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-06 11:02:17 +0300

    add > stream-spring-boot-starter > new collectors

--- stream-spring-boot-starter/src/test/java/com/pavelshapel/stream/spring/boot/starter/util/StreamUtilsTest.java
+++ stream-spring-boot-starter/src/test/java/com/pavelshapel/stream/spring/boot/starter/util/StreamUtilsTest.java
@@ -1,16 +1,16 @@
 package com.pavelshapel.stream.spring.boot.starter.util;
 
 import com.pavelshapel.stream.spring.boot.starter.StreamStarterAutoConfiguration;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -63,5 +63,102 @@ class StreamUtilsTest {
                 .asList()
                 .isNotEmpty()
                 .isEqualTo(list);
+    }
+
+    @Test
+    void toReverseList_WithCTrueParam_ShouldReturnReverseCollection() {
+        List<String> list = Arrays.asList(
+                String.format("%s%d", COLLECTION_ELEMENT, 1),
+                String.format("%s%d", COLLECTION_ELEMENT, 2)
+        );
+
+        List<String> reverseList = list.stream().collect(streamUtils.toList(true));
+
+        assertThat(reverseList)
+                .asList()
+                .isNotEmpty()
+                .isNotEqualTo(list);
+        assertThat(list.get(0))
+                .isEqualTo(reverseList.get(reverseList.size() - 1));
+    }
+
+    @Test
+    void toReverseList_WithCFalseParam_ShouldReturnNotReverseCollection() {
+        List<String> list = Arrays.asList(
+                String.format("%s%d", COLLECTION_ELEMENT, 1),
+                String.format("%s%d", COLLECTION_ELEMENT, 2)
+        );
+
+        List<String> reverseList = list.stream().collect(streamUtils.toList(false));
+
+        assertThat(reverseList)
+                .asList()
+                .isNotEmpty()
+                .isEqualTo(list);
+    }
+
+    @Test
+    void toMapOfNullables_WithoutDuplicateKeysWithoutNullValues_ShouldReturnMapWithBooleans() {
+        Map<String, Boolean> map = Stream.of(Boolean.TRUE, Boolean.FALSE)
+                .collect(streamUtils.toMapOfNullables(Object::toString, Function.identity(), this::and));
+
+        map.values().forEach(value -> assertThat(value).isInstanceOf(Boolean.class));
+    }
+
+    @Test
+    void toMapOfNullables_WithoutDuplicateKeysWithNullValues_ShouldReturnMapWithNulls() {
+        Map<String, Boolean> map = Stream.of(Boolean.TRUE, Boolean.FALSE)
+                .collect(streamUtils.toMapOfNullables(Object::toString, value -> null, this::and));
+
+        map.values().forEach(value -> assertThat(value).isNull());
+    }
+
+    @Test
+    void toMapOfNullables_WithDuplicateKeysWithFalseValues_ShouldReturnMapWithFalse() {
+        Map<String, Boolean> map = Stream.of(Boolean.FALSE, Boolean.FALSE)
+                .collect(streamUtils.toMapOfNullables(Object::toString, Function.identity(), this::and));
+
+        assertThat(map)
+                .asInstanceOf(InstanceOfAssertFactories.map(String.class, Boolean.class))
+                .hasSize(1)
+                .containsEntry(Boolean.FALSE.toString(), Boolean.FALSE);
+    }
+
+    @Test
+    void toMapOfNullables_WithDuplicateKeysWithTrueValues_ShouldReturnMapWithTrue() {
+        Map<String, Boolean> map = Stream.of(Boolean.TRUE, Boolean.TRUE)
+                .collect(streamUtils.toMapOfNullables(Object::toString, Function.identity(), this::and));
+
+        assertThat(map)
+                .asInstanceOf(InstanceOfAssertFactories.map(String.class, Boolean.class))
+                .hasSize(1)
+                .containsEntry(Boolean.TRUE.toString(), Boolean.TRUE);
+    }
+
+    @Test
+    void toMapOfNullables_WithDuplicateKeysWithTrueFalseValues_ShouldReturnMapWithFalse() {
+        Map<String, Boolean> map = Stream.of(Boolean.TRUE, Boolean.FALSE)
+                .collect(streamUtils.toMapOfNullables(value -> Boolean.TRUE.toString(), Function.identity(), this::and));
+
+        assertThat(map)
+                .asInstanceOf(InstanceOfAssertFactories.map(String.class, Boolean.class))
+                .hasSize(1)
+                .containsEntry(Boolean.TRUE.toString(), Boolean.FALSE);
+    }
+
+    @Test
+    void toMapOfNullables_WithDuplicateKeysWithNullValues_ShouldReturnMapWithFalse() {
+        Map<String, Boolean> map = Stream.of(Boolean.TRUE, Boolean.TRUE)
+                .collect(streamUtils.toMapOfNullables(Object::toString, value -> null, this::and));
+
+        assertThat(map)
+                .asInstanceOf(InstanceOfAssertFactories.map(String.class, Boolean.class))
+                .hasSize(1)
+                .containsEntry(Boolean.TRUE.toString(), Boolean.FALSE);
+    }
+
+
+    private boolean and(Boolean existing, Boolean replacement) {
+        return Boolean.TRUE.equals(existing) && Boolean.TRUE.equals(replacement);
     }
 }
