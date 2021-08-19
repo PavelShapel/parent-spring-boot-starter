ceefffc0d86973df4b27950fd4b075f885335f24
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-19 13:59:34 +0300

    refactor > random-spring-boot-starter > RandomContextRefreshedListener.java

--- random-spring-boot-starter/src/main/java/com/pavelshapel/random/spring/boot/starter/RandomContextRefreshedListener.java
+++ random-spring-boot-starter/src/main/java/com/pavelshapel/random/spring/boot/starter/RandomContextRefreshedListener.java
@@ -10,6 +10,6 @@ import static com.pavelshapel.random.spring.boot.starter.StarterAutoConfiguratio
 public class RandomContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        log.info(String.format("%s-spring-boot-starter was applied", TYPE));
+        log.info("{}-spring-boot-starter was applied", TYPE);
     }
 }
