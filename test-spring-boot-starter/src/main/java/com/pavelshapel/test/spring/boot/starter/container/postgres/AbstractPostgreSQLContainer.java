dfbb103beabc78b9ae517761957d97144de5f74a
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-19 14:23:04 +0300

    refactor > test-spring-boot-starter > AbstractPostgreSQLContainer.java

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/postgres/AbstractPostgreSQLContainer.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/postgres/AbstractPostgreSQLContainer.java
@@ -6,12 +6,11 @@ import org.testcontainers.containers.PostgreSQLContainer;
 
 public abstract class AbstractPostgreSQLContainer {
     private static final String POSTGRE_SQL_IMAGE = "postgres:9.6.21";
-    private static final String SCRIPT = "schema.sql";
 
     protected static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER;
 
     static {
-        POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>(POSTGRE_SQL_IMAGE).withInitScript(SCRIPT);
+        POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>(POSTGRE_SQL_IMAGE);
         POSTGRE_SQL_CONTAINER.start();
     }
 
@@ -23,7 +22,7 @@ public abstract class AbstractPostgreSQLContainer {
         registry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
         registry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
-        registry.add("spring.datasource.initialization-mode", () -> "always");
+        registry.add("spring.datasource.initialization-mode", () -> "never");
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "none");
     }
 }
