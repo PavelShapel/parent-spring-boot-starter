fef5917c2b760a6485e58918f909e9817a4aebaa
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-21 12:07:04 +0300

    add > test-spring-boot-starter > PostgreSQLExtension.java, AbstractRepositoryLayerTest.java

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/TestStarterAutoConfiguration.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/TestStarterAutoConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class StarterAutoConfiguration {
+public class TestStarterAutoConfiguration {
     public static final String TYPE = "test";
 
     @Bean
---
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/postgres/PostgreSQLExtension.java
@@ -0,0 +1,36 @@
+package com.pavelshapel.test.spring.boot.starter.container.postgres;
+
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class PostgreSQLExtension implements BeforeAllCallback, AfterAllCallback {
+    public static final String DEFAULT_TAG = "9.6.12";
+    private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("postgres");
+
+    private PostgreSQLContainer<?> postgreSQLContainer;
+
+    @Override
+    public void beforeAll(ExtensionContext context) {
+        postgreSQLContainer = new PostgreSQLContainer<>(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
+
+        postgreSQLContainer.start();
+        System.setProperty("spring.datasource.url", postgreSQLContainer.getJdbcUrl());
+        System.setProperty("spring.datasource.username", postgreSQLContainer.getUsername());
+        System.setProperty("spring.datasource.password", postgreSQLContainer.getPassword());
+        System.setProperty("spring.datasource.initialization-mode", "never");
+        System.setProperty("spring.jpa.hibernate.ddl-auto", "none");
+        System.setProperty("spring.liquibase.enabled", "true");
+        System.setProperty("spring.liquibase.url", postgreSQLContainer.getJdbcUrl());
+        System.setProperty("spring.liquibase.user", postgreSQLContainer.getUsername());
+        System.setProperty("spring.liquibase.password", postgreSQLContainer.getPassword());
+        System.setProperty("spring.liquibase.change-log", "classpath:db/changelog/db.changelog-master.yml");
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) {
+        postgreSQLContainer.stop();
+    }
+}
