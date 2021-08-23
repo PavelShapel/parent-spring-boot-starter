17c3061ed4fb16ae5ba59391f896722ccfd90928
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-23 15:52:24 +0300

    remove > test-spring-boot-starter > StartPostgreSQLContainer.java

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/layer/AbstractRepositoryTest.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/layer/AbstractRepositoryTest.java
@@ -6,21 +6,26 @@ import com.pavelshapel.jpa.spring.boot.starter.repository.AbstractJpaRepository;
 import com.pavelshapel.jpa.spring.boot.starter.repository.search.SearchCriteria;
 import com.pavelshapel.jpa.spring.boot.starter.repository.search.SearchOperation;
 import com.pavelshapel.jpa.spring.boot.starter.repository.search.SearchSpecification;
-import com.pavelshapel.test.spring.boot.starter.annotation.StartPostgreSQLContainer;
+import com.pavelshapel.test.spring.boot.starter.container.postgres.PostgreSQLExtension;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ContextConfiguration;
+
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 
 @DataJpaTest
 @ContextConfiguration(classes = {
         JpaAuditingConfiguration.class
 })
-@StartPostgreSQLContainer
+@AutoConfigureTestDatabase(replace = NONE)
+@ExtendWith(PostgreSQLExtension.class)
 @Getter(AccessLevel.PROTECTED)
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AbstractRepositoryTest<T extends AbstractEntity> {
