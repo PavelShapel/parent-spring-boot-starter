8772ce0b215006e4509e5bb8c224ad53fe59f5f3
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-24 09:53:24 +0300

    refactor > core-spring-boot-starter > CoreContextRefreshedListener.java

--- core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/CoreContextRefreshedListener.java
+++ core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/CoreContextRefreshedListener.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
-import static com.pavelshapel.core.spring.boot.starter.StarterAutoConfiguration.TYPE;
+import static com.pavelshapel.core.spring.boot.starter.CoreStarterAutoConfiguration.TYPE;
 
 @Slf4j
 public class CoreContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
--- core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/CoreStarterAutoConfiguration.java
+++ core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/CoreStarterAutoConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class StarterAutoConfiguration {
+public class CoreStarterAutoConfiguration {
     public static final String TYPE = "core";
 
     @Bean
