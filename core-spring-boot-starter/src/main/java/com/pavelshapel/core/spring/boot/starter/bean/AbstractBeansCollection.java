6162e835f0f8374b4e2a19b60a487373751c0431
Pavel, Shapel <pavel.shapel@ukg.com>
2021-09-19 16:32:53 +0300

    refactor > core-spring-boot-starter > AbstractBeansCollection.java

--- core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/bean/AbstractBeansCollection.java
+++ core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/bean/AbstractBeansCollection.java
@@ -4,16 +4,24 @@ import com.pavelshapel.stream.spring.boot.starter.util.StreamUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
 
 public abstract class AbstractBeansCollection<T> implements BeansCollection<T> {
     @Autowired
     private StreamUtils streamUtils;
+    @Autowired
+    private Map<String, T> beans;
+
+    @Override
+    public Map<String, T> getBeans() {
+        return beans;
+    }
 
     @Override
     public Optional<T> getBean(String beanName) {
-        return Optional.ofNullable(getBeans().get(beanName));
+        return Optional.ofNullable(beans.get(beanName));
     }
 
     @Override
@@ -24,7 +32,7 @@ public abstract class AbstractBeansCollection<T> implements BeansCollection<T> {
 
     @Override
     public Optional<T> getBean(Predicate<T> predicate) {
-        return getBeans().values().stream()
+        return beans.values().stream()
                 .filter(predicate)
                 .collect(streamUtils.toSingleton());
     }
