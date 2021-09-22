72c22dd20ce0f7bf4a582229cc1ce4eaaef83632
Pavel, Shapel <pavel.shapel@ukg.com>
2021-09-22 20:59:44 +0300

    json-spring-boot-starter: refactor

--- json-spring-boot-starter/src/test/java/com/pavelshapel/json/spring/boot/starter/converter/jackson/JacksonJsonConverterTest.java
+++ json-spring-boot-starter/src/test/java/com/pavelshapel/json/spring/boot/starter/converter/jackson/JacksonJsonConverterTest.java
@@ -4,14 +4,9 @@ import com.pavelshapel.json.spring.boot.starter.JsonStarterAutoConfiguration;
 import com.pavelshapel.json.spring.boot.starter.converter.JsonConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
-@ContextConfiguration(classes = {
-        JsonStarterAutoConfiguration.class
-})
+@SpringBootTest(classes = {JsonStarterAutoConfiguration.class})
 class JacksonJsonConverterTest extends AbstractJsonConverterTest {
-
     @Autowired
     protected JacksonJsonConverterTest(JsonConverter jacksonJsonConverter) {
         super(jacksonJsonConverter);
