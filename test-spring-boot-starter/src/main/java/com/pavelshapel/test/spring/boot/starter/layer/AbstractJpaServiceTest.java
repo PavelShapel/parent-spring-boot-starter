6cbde68d8b54b3eb10215fb5252b95e0ebdc3d50
Pavel, Shapel <pavel.shapel@ukg.com>
2021-09-23 16:56:53 +0300

    test-spring-boot-starter: refactor

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/layer/AbstractJpaServiceTest.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/layer/AbstractJpaServiceTest.java
@@ -2,16 +2,11 @@ package com.pavelshapel.test.spring.boot.starter.layer;
 
 import com.pavelshapel.jpa.spring.boot.starter.entity.AbstractEntity;
 import com.pavelshapel.jpa.spring.boot.starter.repository.search.SearchSpecification;
-import lombok.AccessLevel;
-import lombok.Getter;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@Getter(AccessLevel.PROTECTED)
 @ExtendWith(MockitoExtension.class)
 public abstract class AbstractJpaServiceTest<T extends AbstractEntity> {
 
