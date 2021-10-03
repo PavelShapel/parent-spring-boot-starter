bf98a72f0d75b8bade3256cc5e96e657ffabc771
Pavel, Shapel <pavel.shapel@ukg.com>
2021-10-03 17:49:00 +0300

    web-spring-boot-starter: add AbstractDto.java

--- web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/web/converter/ToDtoConverter.java
+++ web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/web/converter/ToDtoConverter.java
@@ -1,6 +1,7 @@
 package com.pavelshapel.web.spring.boot.starter.web.converter;
 
 import com.pavelshapel.jpa.spring.boot.starter.entity.AbstractEntity;
+import org.springframework.core.convert.converter.Converter;
 
-public abstract class ToDtoConverter<T extends AbstractEntity> implements DtoConverter<T> {
+public abstract class ToDtoConverter<S extends AbstractEntity, T extends AbstractDto> implements Converter<S, T> {
 }
