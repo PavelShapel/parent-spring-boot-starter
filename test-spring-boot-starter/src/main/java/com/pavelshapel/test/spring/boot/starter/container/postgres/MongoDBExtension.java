ffe67981652681c0cbb195a957ba6c44cfb5d6bb
Pavel, Shapel <pavel.shapel@ukg.com>
2021-10-17 13:05:41 +0300

    test-spring-boot-starter: add MongoDBExtension.java

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/postgres/MongoDBExtension.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/postgres/MongoDBExtension.java
@@ -9,10 +9,6 @@ import org.testcontainers.utility.DockerImageName;
 public class MongoDBExtension implements BeforeAllCallback, AfterAllCallback {
     private static final String DEFAULT_TAG = "5.0.3";
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("mongo");
-    private static final String MONGO_INITDB_DATABASE = "test";
-    private static final String MONGO_INITDB_ROOT_USERNAME = "admin";
-    private static final String MONGO_INITDB_ROOT_PASSWORD = "secret";
-    private static final String MONGO_URI_TEMPLATE = "mongodb://%s:%s@%s:%d/%s";
     private static final String MONGO_URI_PROPERTY_PATH = "spring.data.mongodb.uri";
 
     private MongoDBContainer mongoDBContainer;
@@ -20,17 +16,8 @@ public class MongoDBExtension implements BeforeAllCallback, AfterAllCallback {
     @Override
     public void beforeAll(ExtensionContext context) {
         mongoDBContainer = new MongoDBContainer(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
-        mongoDBContainer.addEnv("MONGO_INITDB_ROOT_USERNAME", MONGO_INITDB_ROOT_USERNAME);
-        mongoDBContainer.addEnv("MONGO_INITDB_ROOT_PASSWORD", MONGO_INITDB_ROOT_PASSWORD);
         mongoDBContainer.start();
-        String uri = String.format(
-                MONGO_URI_TEMPLATE,
-                MONGO_INITDB_ROOT_USERNAME,
-                MONGO_INITDB_ROOT_PASSWORD,
-                mongoDBContainer.getHost(),
-                mongoDBContainer.getFirstMappedPort(),
-                MONGO_INITDB_DATABASE);
-        System.setProperty(MONGO_URI_PROPERTY_PATH, uri);
+        System.setProperty(MONGO_URI_PROPERTY_PATH, mongoDBContainer.getReplicaSetUrl());
     }
 
     @Override
