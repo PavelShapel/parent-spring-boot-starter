327e18bf0bcb7f236052cc4d9f7d75841e9c8ece
Pavel, Shapel <pavel.shapel@ukg.com>
2021-10-23 17:51:11 +0300

    test-spring-boot-starter: add ElasticsearchExtension.java

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/ElasticsearchExtension.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/ElasticsearchExtension.java
@@ -7,21 +7,17 @@ import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.utility.DockerImageName;
 
 public class ElasticsearchExtension implements BeforeAllCallback, AfterAllCallback {
-    private static final String ELASTICSEARCH = "elasticsearch";
-    private static final String DEFAULT_TAG = "6.8.20";
-    private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("elasticsearch").asCompatibleSubstituteFor("docker.elastic.co/elasticsearch/elasticsearch");
+    private static final String DEFAULT_TAG = "7.15.1";
+    private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch");
     private static final String ELASTICSEARCH_SERVER = "elasticsearch.server";
-    private static final String DISCOVERY_TYPE = "discovery.type";
-    private static final String SINGLE_NODE = "single-node";
-    private static final String CLUSTER_NAME = "cluster.name";
+    private static final String X_PACK_SECURITY_ENABLED = "xpack.security.enabled";
 
     private ElasticsearchContainer elasticsearchContainer;
 
     @Override
     public void beforeAll(ExtensionContext context) {
         elasticsearchContainer = new ElasticsearchContainer(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
-        elasticsearchContainer.addEnv(DISCOVERY_TYPE, SINGLE_NODE);
-        elasticsearchContainer.addEnv(CLUSTER_NAME, ELASTICSEARCH);
+        elasticsearchContainer.addEnv(X_PACK_SECURITY_ENABLED, Boolean.FALSE.toString());
         elasticsearchContainer.start();
         System.setProperty(ELASTICSEARCH_SERVER, elasticsearchContainer.getHttpHostAddress());
     }
