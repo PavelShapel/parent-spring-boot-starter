66ff9f57c90962ca3a99067d55935fa6e9ac1929
Pavel, Shapel <pavel.shapel@ukg.com>
2021-11-06 09:48:31 +0300

    test-spring-boot-starter: refactor

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/KafkaExtension.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/KafkaExtension.java
@@ -9,7 +9,12 @@ import org.testcontainers.utility.DockerImageName;
 public class KafkaExtension implements BeforeAllCallback, AfterAllCallback {
     private static final String DEFAULT_TAG = "6.2.1";
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("confluentinc/cp-kafka");
-    private static final String KAFKA_SERVER_PROPERTY_PATH = "kafka.server";
+    private static final String KAFKA_SERVER_PROPERTY_PATH = "spring.kafka.server";
+    private static final String KAFKA_CONSUMER_AUTO_OFFSET_RESET_PATH = "spring.kafka.consumer.auto-offset-reset";
+    private static final String KAFKA_CONSUMER_GROUP_ID_PATH = "spring.kafka.consumer.group-id";
+    private static final String KAFKA_ENABLE_AUTO_COMMIT_PATH = "spring.kafka.consumer.enable-auto-commit";
+    private static final String EARLIEST = "earliest";
+    private static final String GROUP_ID = "kafka-test";
 
     private KafkaContainer kafkaContainer;
 
@@ -18,6 +23,9 @@ public class KafkaExtension implements BeforeAllCallback, AfterAllCallback {
         kafkaContainer = new KafkaContainer(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
         kafkaContainer.start();
         System.setProperty(KAFKA_SERVER_PROPERTY_PATH, kafkaContainer.getBootstrapServers());
+        System.setProperty(KAFKA_CONSUMER_GROUP_ID_PATH, GROUP_ID);
+        System.setProperty(KAFKA_CONSUMER_AUTO_OFFSET_RESET_PATH, EARLIEST);
+        System.setProperty(KAFKA_ENABLE_AUTO_COMMIT_PATH, "false");
     }
 
     @Override
