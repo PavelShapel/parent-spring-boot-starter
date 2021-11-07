4d933f32e282b8f09a9bf9a8c7b4af103252d23b
Pavel, Shapel <pavel.shapel@ukg.com>
2021-11-07 12:07:27 +0300

    test-spring-boot-starter: refactor

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/KafkaExtension.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/KafkaExtension.java
@@ -12,7 +12,6 @@ public class KafkaExtension implements BeforeAllCallback, AfterAllCallback {
     private static final String KAFKA_SERVER_PROPERTY_PATH = "spring.kafka.server";
     private static final String KAFKA_CONSUMER_AUTO_OFFSET_RESET_PATH = "spring.kafka.consumer.auto-offset-reset";
     private static final String KAFKA_CONSUMER_GROUP_ID_PATH = "spring.kafka.consumer.group-id";
-    private static final String KAFKA_ENABLE_AUTO_COMMIT_PATH = "spring.kafka.consumer.enable-auto-commit";
     private static final String EARLIEST = "earliest";
     private static final String GROUP_ID = "kafka-test";
 
@@ -25,7 +24,6 @@ public class KafkaExtension implements BeforeAllCallback, AfterAllCallback {
         System.setProperty(KAFKA_SERVER_PROPERTY_PATH, kafkaContainer.getBootstrapServers());
         System.setProperty(KAFKA_CONSUMER_GROUP_ID_PATH, GROUP_ID);
         System.setProperty(KAFKA_CONSUMER_AUTO_OFFSET_RESET_PATH, EARLIEST);
-        System.setProperty(KAFKA_ENABLE_AUTO_COMMIT_PATH, "false");
     }
 
     @Override
