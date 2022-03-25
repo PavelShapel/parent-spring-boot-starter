87c1d70b80c553354df171d42eb68cf42de5ba7f
Pavel, Shapel <pavel.shapel@ukg.com>
2022-03-25 11:40:48 +0300

    cache-spring-boot-starter: change logger

--- cache-spring-boot-starter/src/main/java/com/pavelshapel/cache/spring/boot/starter/CacheContextRefreshedListener.java
+++ cache-spring-boot-starter/src/main/java/com/pavelshapel/cache/spring/boot/starter/CacheContextRefreshedListener.java
@@ -1,17 +1,19 @@
 package com.pavelshapel.cache.spring.boot.starter;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.java.Log;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 import static com.pavelshapel.cache.spring.boot.starter.CacheStarterAutoConfiguration.TYPE;
+import static java.util.logging.Level.INFO;
 
-@Slf4j
+@SuppressWarnings("NullableProblems")
+@Log
 public class CacheContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
     public static final String LOG_PATTERN = "{}-spring-boot-starter was applied";
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        log.info(LOG_PATTERN, TYPE);
+        log.log(INFO, LOG_PATTERN, TYPE);
     }
 }
