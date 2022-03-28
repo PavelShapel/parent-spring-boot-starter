f6e1749c58672f78e569339cd087c5b9f45ab84f
Pavel, Shapel <pavel.shapel@ukg.com>
2022-03-28 10:26:11 +0300

    kafka-spring-boot-starter: refactor

--- kafka-spring-boot-starter/src/main/java/com/pavelshapel/kafka/spring/boot/starter/KafkaContextRefreshedListener.java
+++ kafka-spring-boot-starter/src/main/java/com/pavelshapel/kafka/spring/boot/starter/KafkaContextRefreshedListener.java
@@ -1,16 +1,20 @@
 package com.pavelshapel.kafka.spring.boot.starter;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.java.Log;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 import static com.pavelshapel.kafka.spring.boot.starter.KafkaStarterAutoConfiguration.TYPE;
+import static java.util.logging.Level.INFO;
 
 
-@Slf4j
+@SuppressWarnings("NullableProblems")
+@Log
 public class KafkaContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
+    public static final String LOG_PATTERN = "{0}-spring-boot-starter was applied";
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        log.info("{}-spring-boot-starter was applied", TYPE);
+        log.log(INFO, LOG_PATTERN, TYPE);
     }
 }
