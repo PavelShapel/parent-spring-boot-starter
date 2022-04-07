b441d4b50a69aec9e05065c4619c27b18817e4a5
Pavel, Shapel <pavel.shapel@ukg.com>
2022-04-07 18:17:07 +0300

    core-spring-boot-starter: refactor

--- core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/api/model/Typed.java
+++ core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/api/model/Typed.java
@@ -1,6 +1,8 @@
 package com.pavelshapel.core.spring.boot.starter.api.model;
 
 public interface Typed {
+    String TYPE = "type";
+
     String getType();
 
     void setType(String type);
--- core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/api/model/Versioned.java
+++ core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/api/model/Versioned.java
@@ -1,6 +1,8 @@
 package com.pavelshapel.core.spring.boot.starter.api.model;
 
 public interface Versioned {
+    String VERSION = "version";
+
     Long getVersion();
 
     void setVersion(Long version);
