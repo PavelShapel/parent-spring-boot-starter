b09d82d4a3be9c1d1d31dda898e544998c1836c0
Pavel, Shapel <pavel.shapel@ukg.com>
2022-04-15 11:57:36 +0300

    core-spring-boot-starter: refactor TypedDto.java

--- core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/impl/model/TypedDto.java
+++ core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/impl/model/TypedDto.java
@@ -2,13 +2,20 @@ package com.pavelshapel.core.spring.boot.starter.impl.model;
 
 import com.pavelshapel.core.spring.boot.starter.api.model.Typed;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = false)
-public class TypedDto extends HashMap<String, Object> implements Typed<String> {
+@NoArgsConstructor
+public class TypedDto extends LinkedHashMap<String, Object> implements Typed<String> {
+    public TypedDto(Map<String, Object> map) {
+        super(map);
+    }
+
     @Override
     public String getType() {
         return get(TYPE_FIELD).toString();
