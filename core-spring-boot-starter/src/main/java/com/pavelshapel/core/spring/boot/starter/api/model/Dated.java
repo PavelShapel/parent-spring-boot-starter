f7c7d655513f8b9ba0b8de97029b70754d352e99
Pavel, Shapel <pavel.shapel@ukg.com>
2022-04-16 15:51:05 +0300

    parent-spring-boot-starter: refactor

--- core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/api/model/Dated.java
+++ core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/api/model/Dated.java
@@ -3,6 +3,9 @@ package com.pavelshapel.core.spring.boot.starter.api.model;
 import java.util.Date;
 
 public interface Dated {
+    String CREATED_DATE = "createdDate";
+    String LAST_MODIFIED_DATE = "lastModifiedDate";
+
     Date getCreatedDate();
 
     void setCreatedDate(Date createdDate);
