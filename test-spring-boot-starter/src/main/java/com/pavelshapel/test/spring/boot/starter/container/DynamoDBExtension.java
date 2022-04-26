3d8ea1d9690b7e4f8075598297ad905640caddbf
Pavel, Shapel <pavel.shapel@ukg.com>
2022-04-26 13:40:28 +0300

    test-spring-boot-starter: refactor DynamoDBExtension.java

--- test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/DynamoDBExtension.java
+++ test-spring-boot-starter/src/main/java/com/pavelshapel/test/spring/boot/starter/container/DynamoDBExtension.java
@@ -10,7 +10,9 @@ public class DynamoDBExtension implements BeforeAllCallback, AfterAllCallback {
     private static final String DEFAULT_TAG = "v1.2.1-1";
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("quay.io/testcontainers/dynalite");
     private static final String AWS_PROFILE_PATH = "spring.aws.profile";
-    private static final String AWS_ENDPOINT_PATH = "spring.aws.endpoint";
+    private static final String AWS_DYNAMO_DB_PATH = "spring.aws.dynamo-db";
+    private static final String AWS_DYNAMO_DB_ENABLE_PATH = String.format("%s.enable", AWS_DYNAMO_DB_PATH);
+    private static final String AWS_DYNAMO_DB_ENDPOINT_PATH = String.format("%s.endpoint", AWS_DYNAMO_DB_PATH);
 
     private DynaliteContainer dynamoDBContainer;
 
@@ -19,7 +21,8 @@ public class DynamoDBExtension implements BeforeAllCallback, AfterAllCallback {
         dynamoDBContainer = new DynaliteContainer(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
         dynamoDBContainer.start();
         System.setProperty(AWS_PROFILE_PATH, "default");
-        System.setProperty(AWS_ENDPOINT_PATH, dynamoDBContainer.getEndpointConfiguration().getServiceEndpoint());
+        System.setProperty(AWS_DYNAMO_DB_ENABLE_PATH, "true");
+        System.setProperty(AWS_DYNAMO_DB_ENDPOINT_PATH, dynamoDBContainer.getEndpointConfiguration().getServiceEndpoint());
     }
 
     @Override
