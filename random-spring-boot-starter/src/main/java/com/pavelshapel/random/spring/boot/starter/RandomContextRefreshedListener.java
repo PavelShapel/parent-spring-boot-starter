5edb7e264675981045dda3702c7b7060ac616ae1
Pavel, Shapel <pavel.shapel@ukg.com>
2022-05-09 10:02:52 +0300

    random-spring-boot-starter: refactor

--- random-spring-boot-starter/src/main/java/com/pavelshapel/random/spring/boot/starter/RandomContextRefreshedListener.java
+++ random-spring-boot-starter/src/main/java/com/pavelshapel/random/spring/boot/starter/RandomContextRefreshedListener.java
@@ -3,16 +3,16 @@ package com.pavelshapel.random.spring.boot.starter;
 import lombok.extern.java.Log;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.lang.NonNull;
 
 import java.util.logging.Level;
 
 import static com.pavelshapel.random.spring.boot.starter.RandomStarterAutoConfiguration.TYPE;
 
-@SuppressWarnings("NullableProblems")
 @Log
 public class RandomContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+    public void onApplicationEvent(@NonNull ContextRefreshedEvent contextRefreshedEvent) {
         log.log(Level.INFO, "{0}-spring-boot-starter was applied", TYPE);
     }
 }
