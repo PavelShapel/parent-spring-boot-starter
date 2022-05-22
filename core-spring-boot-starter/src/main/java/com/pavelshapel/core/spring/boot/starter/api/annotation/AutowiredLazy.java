a3b3ce138bd4f2bab38c4c6740bd0f776eebf667
Pavel, Shapel <pavel.shapel@ukg.com>
2022-05-22 13:29:35 +0300

    core-spring-boot-starter: add new annotation [AutowiredLazy.java]

---
+++ core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/api/annotation/AutowiredLazy.java
@@ -0,0 +1,15 @@
+package com.pavelshapel.core.spring.boot.starter.api.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Autowired
+@Lazy
+public @interface AutowiredLazy {
+}
