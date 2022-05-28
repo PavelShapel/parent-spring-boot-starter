d21a66639389b6ec43a28c3329826b4382946665
Pavel, Shapel <pavel.shapel@ukg.com>
2022-05-28 09:56:12 +0300

    web-spring-boot-starter: refactor

--- web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/wrapper/TypedResponseWrapperRestControllerAdvice.java
+++ web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/wrapper/TypedResponseWrapperRestControllerAdvice.java
@@ -4,22 +4,24 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @ControllerAdvice(annotations = TypedResponseWrapperRestController.class)
 public class TypedResponseWrapperRestControllerAdvice implements ResponseBodyAdvice<Object> {
     @Override
-    public boolean supports(MethodParameter methodParameter, Class aClass) {
+    public boolean supports(@NonNull MethodParameter methodParameter,@NonNull Class aClass) {
         return true;
     }
 
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter methodParameter,
-                                  MediaType mediaType,
-                                  Class aClass,
-                                  ServerHttpRequest serverHttpRequest,
-                                  ServerHttpResponse serverHttpResponse) {
+    public Object beforeBodyWrite(Object body,
+                                  @NonNull MethodParameter methodParameter,
+                                  @NonNull MediaType mediaType,
+                                  @NonNull Class aClass,
+                                  @NonNull ServerHttpRequest serverHttpRequest,
+                                  @NonNull ServerHttpResponse serverHttpResponse) {
         return new TypedResponseWrapper(body, body.getClass().getSimpleName());
     }
 }
