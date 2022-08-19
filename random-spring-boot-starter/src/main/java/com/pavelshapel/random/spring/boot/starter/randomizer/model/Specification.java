d37cfca10fd1e3b627b61fc9b7876b2cce946460
Pavel, Shapel <pavel.shapel@ukg.com>
2022-08-19 12:44:23 +0200

    random-spring-boot-starter: refactor Specification.java

--- random-spring-boot-starter/src/main/java/com/pavelshapel/random/spring/boot/starter/randomizer/model/Specification.java
+++ random-spring-boot-starter/src/main/java/com/pavelshapel/random/spring/boot/starter/randomizer/model/Specification.java
@@ -1,19 +1,11 @@
 package com.pavelshapel.random.spring.boot.starter.randomizer.model;
 
-import com.pavelshapel.core.spring.boot.starter.api.model.Typed;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.FieldDefaults;
+import lombok.Value;
 
-@Data
+@Value
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
-public class Specification implements Typed<String> {
+public class Specification {
     String type;
     long min;
     long max;
