3e323b501db0ab17062cb3c88bb9566f2b41737e
Pavel, Shapel <pavel.shapel@ukg.com>
2022-11-27 10:36:30 +0100

    web-spring-boot-starter: refactoring

--- web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/web/AbstractDaoRestController.java
+++ web-spring-boot-starter/src/main/java/com/pavelshapel/web/spring/boot/starter/web/AbstractDaoRestController.java
@@ -1,6 +1,6 @@
 package com.pavelshapel.web.spring.boot.starter.web;
 
-import com.pavelshapel.aop.spring.boot.starter.log.method.Loggable;
+import com.pavelshapel.aop.spring.boot.starter.annotation.Loggable;
 import com.pavelshapel.core.spring.boot.starter.api.converter.FromDtoConverter;
 import com.pavelshapel.core.spring.boot.starter.api.converter.ToDtoConverter;
 import com.pavelshapel.core.spring.boot.starter.api.model.Dto;
@@ -14,7 +14,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
