0a37e75e48b29581aa92756054333974cd1072aa
Pavel, Shapel <pavel.shapel@ukg.com>
2022-11-27 10:38:36 +0100

    jpa-spring-boot-starter: refactoring

--- jpa-spring-boot-starter/src/main/java/com/pavelshapel/jpa/spring/boot/starter/repository/DaoRepository.java
+++ jpa-spring-boot-starter/src/main/java/com/pavelshapel/jpa/spring/boot/starter/repository/DaoRepository.java
@@ -1,7 +1,12 @@
 package com.pavelshapel.jpa.spring.boot.starter.repository;
 
 import com.pavelshapel.core.spring.boot.starter.api.model.Entity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface DaoRepository<ID, T extends Entity<ID>> extends PagingAndSortingRepository<T, ID> {
+    @Query("select t from #{#entityName} t where t.parent = ?1")
+    List<T> getChildren(T entity);
 }
