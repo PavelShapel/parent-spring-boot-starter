9d44c36a27f2ca776eeddee8e82931d1e9ec11fd
Pavel, Shapel <pavel.shapel@ukg.com>
2023-02-27 13:13:02 +0100

    core-spring-boot-starter: add ROOT_ID constant to ParentalEntity.java

--- core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/api/model/ParentalEntity.java
+++ core-spring-boot-starter/src/main/java/com/pavelshapel/core/spring/boot/starter/api/model/ParentalEntity.java
@@ -2,6 +2,7 @@ package com.pavelshapel.core.spring.boot.starter.api.model;
 
 public interface ParentalEntity<ID, T extends ParentalEntity<ID, T>> extends Entity<ID> {
     String PARENT_FIELD = "parent";
+    String ROOT_ID = String.format("%s#null", PARENT_FIELD);
 
     T getParent();
 
