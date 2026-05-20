9f1ae99e61321e4cda141ba8c9d73b6fd5e2545f
Pavel Shapel <pavel.shapel.freelancer@gmail.com>
2026-05-20 17:24:43 +0200

    bot-api-spring-boot-starter: change Worker class to implement Consumer interface

--- bot-api-spring-boot-starter/src/main/java/com/pavelshapel/bot/api/spring/boot/starter/Worker.java
+++ bot-api-spring-boot-starter/src/main/java/com/pavelshapel/bot/api/spring/boot/starter/Worker.java
@@ -1,14 +1,6 @@
 package com.pavelshapel.bot.api.spring.boot.starter;
 
 import com.pavelshapel.bot.api.spring.boot.starter.model.context.ContextRegistry;
-import com.pavelshapel.ordered.spring.boot.starter.OrderedComponent;
+import java.util.function.Consumer;
 
-public abstract class Worker extends OrderedComponent<ContextRegistry, Void> {
-  @Override
-  public final Void apply(ContextRegistry contextRegistry) {
-    doWork(contextRegistry);
-    return null;
-  }
-
-  protected abstract void doWork(ContextRegistry contextRegistry);
-}
+public abstract class Worker implements Consumer<ContextRegistry> {}
