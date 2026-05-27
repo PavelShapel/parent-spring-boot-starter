aade6e1c14e4e4a48621445313329cd4b98e3de3
Pavel Shapel <pavel.shapel.freelancer@gmail.com>
2026-05-27 16:08:50 +0200

    bot-api-spring-boot-starter: improve MessageContext.java by "with" methods

--- bot-api-spring-boot-starter/src/main/java/com/pavelshapel/bot/api/spring/boot/starter/model/context/MessageContext.java
+++ bot-api-spring-boot-starter/src/main/java/com/pavelshapel/bot/api/spring/boot/starter/model/context/MessageContext.java
@@ -3,4 +3,17 @@ package com.pavelshapel.bot.api.spring.boot.starter.model.context;
 import java.util.Set;
 
 public record MessageContext(Long socialId, String message, Set<UserContext> newChatMembers)
-    implements Context {}
+    implements Context {
+
+  public MessageContext withSocialId(Long socialId) {
+    return new MessageContext(socialId, this.message, this.newChatMembers);
+  }
+
+  public MessageContext withMessage(String message) {
+    return new MessageContext(this.socialId, message, this.newChatMembers);
+  }
+
+  public MessageContext withNewChatMembers(Set<UserContext> newChatMembers) {
+    return new MessageContext(this.socialId, this.message, newChatMembers);
+  }
+}
