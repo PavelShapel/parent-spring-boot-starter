503bc99552b0cfbb10d658f03c6c5f457bfa480e
Pavel Shapel <pavel.shapel.freelancer@gmail.com>
2026-06-18 13:37:00 +0200

    bot-api-spring-boot-starter: update ContextRegistry.java

--- bot-api-spring-boot-starter/src/main/java/com/pavelshapel/starter/boot/spring/bot/api/model/context/ContextRegistry.java
+++ bot-api-spring-boot-starter/src/main/java/com/pavelshapel/starter/boot/spring/bot/api/model/context/ContextRegistry.java
@@ -1,6 +1,8 @@
 package com.pavelshapel.starter.boot.spring.bot.api.model.context;
 
+import com.pavelshapel.starter.boot.spring.bot.api.model.KeyboardButton;
 import java.util.HashMap;
+import java.util.function.Consumer;
 
 public record ContextRegistry(HashMap<String, Context> contexts) {
   public ContextRegistry add(Context context) {
@@ -9,8 +11,21 @@ public record ContextRegistry(HashMap<String, Context> contexts) {
   }
 
   @SuppressWarnings("unchecked")
-  public <C extends Context> C get(Class<C> contextClass) {
-    return (C) contexts.get(getKey(contextClass));
+  public <CONTEXT extends Context> CONTEXT get(Class<CONTEXT> contextClass) {
+    return (CONTEXT) contexts.get(getKey(contextClass));
+  }
+
+  public ContextRegistry updateMessageText(String text) {
+    return add(get(MessageContext.class).withMessage(text));
+  }
+
+  public <BUTTON extends KeyboardButton> ContextRegistry addButton(BUTTON keyboardButton) {
+    return add(get(KeyboardButtonContext.class).withButton(keyboardButton));
+  }
+
+  public ContextRegistry apply(Consumer<ContextRegistry> action) {
+    action.accept(this);
+    return this;
   }
 
   private static <C extends Context> String getKey(Class<C> contextClass) {
