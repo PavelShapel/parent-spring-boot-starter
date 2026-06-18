39346aee91b2d44817100c1e306f05b9af76e266
Pavel Shapel <pavel.shapel.freelancer@gmail.com>
2026-06-18 13:48:25 +0200

    bot-api-spring-boot-starter: add visibleInListenerSimpleNames method to KeyboardButton.java

--- bot-api-spring-boot-starter/src/main/java/com/pavelshapel/starter/boot/spring/bot/api/model/KeyboardButton.java
+++ bot-api-spring-boot-starter/src/main/java/com/pavelshapel/starter/boot/spring/bot/api/model/KeyboardButton.java
@@ -1,12 +1,15 @@
 package com.pavelshapel.starter.boot.spring.bot.api.model;
 
 import java.util.Comparator;
+import java.util.Set;
 import org.jspecify.annotations.NonNull;
 
 public interface KeyboardButton extends Comparable<KeyboardButton> {
   int row();
 
   int col();
+
+  Set<String> visibleInListenerSimpleNames();
 
   @Override
   default int compareTo(@NonNull KeyboardButton other) {
